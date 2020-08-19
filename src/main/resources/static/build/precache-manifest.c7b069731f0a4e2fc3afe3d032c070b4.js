d6bc8029bb4ba2779a4a79a27d02df0847c09981
Felipe Cardozo <felipe.cardozo@gmail.com>
2020-08-18 22:22:37 -0500

    adds frontend using react

---
+++ src/main/resources/static/build/precache-manifest.c7b069731f0a4e2fc3afe3d032c070b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff59745e790d8cada970c712549e3ee6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d4fddb6ff356ac7dfdd8",
+    "url": "./static/css/main.005d9b7c.chunk.css"
+  },
+  {
+    "revision": "86d59ca6d76708eb2f0b",
+    "url": "./static/js/2.f0141af3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.f0141af3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4fddb6ff356ac7dfdd8",
+    "url": "./static/js/main.968d25f6.chunk.js"
+  },
+  {
+    "revision": "803cc1ab242cd16fdd05",
+    "url": "./static/js/runtime-main.8a4b7bf2.js"
+  }
+]);
